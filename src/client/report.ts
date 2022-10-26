eee5ad2a0b8a71a135c8ec277c57e92c5490cba9
venandi48 <bow999@naver.com>
2022-10-26 23:36:59 +0900

    보고서 레이아웃 first commit

--- src/client/report.ts
+++ src/client/report.ts
@@ -1,9 +1,8 @@
 import "../css/common.css";
+import "../css/report.css";
 import mainIcon from "../img/mainIcon.png";
 
 document.addEventListener('DOMContentLoaded', () => {
     const icon = document.getElementById("icon") as HTMLImageElement;
     icon.src = mainIcon;
-
-    
 });
