2b3ac3812e65e353daa6ed2293aaaebd73dd716d
minju <rlaalswn3282@kookmin.ac.kr>
2022-10-26 15:52:41 +0900

    Update css, image loader

--- src/client/index.ts
+++ src/client/index.ts
@@ -1,9 +1,18 @@
-const btnCreate = document.getElementById("btnCreate")
-btnCreate?.addEventListener("click", (ev: Event) => {
+import "../css/index.css";
+import mainIcon from "../img/mainIcon.png";
+
+
+window.addEventListener('DOMContentLoaded', () => {
+    const icon = document.getElementById("icon") as HTMLImageElement;
+    icon.src = mainIcon;
+});
+
+const btnCreate = document.getElementById("btnCreate") as HTMLButtonElement;
+btnCreate.addEventListener("click", (e: Event) => {
     openModal();
 })
 
 function openModal() {
     const modal = document.querySelector(".modal");
-    modal?.classList.remove("hidden")
+    modal?.classList.remove("hidden");
 }
